eadc39852c1b9f251f69b54325deee1cfd701073
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-12 18:34:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-if fizzbuzz() % 3 == 0 && int % 5 == 0
+if fizzbuzz(15) % 3 == 0 && int % 5 == 0
   puts "FizzBuzz"
-elsif fizzbuzz() % 3 == 0 # if the number int is divisible by 3
+elsif fizzbuzz(3) % 3 == 0 # if the number int is divisible by 3
   puts "Fizz" # Go fizz
-elsif fizzbuzz() % 5 == 0
+elsif fizzbuzz(5) % 5 == 0
   puts "Buzz"
-elsif fizzbuzz() % 4 == 0
+elsif fizzbuzz(4) % 4 == 0
   puts "nil"
 
   end
