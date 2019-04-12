603e4f5c5d2656336ef479acc29104a4135487e6
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-12 18:28:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+int = user_input
+user_input =gets
+
+
 def fizzbuzz(int)
 if int % 3 == 0 && int % 5 == 0
   puts "FizzBuzz"
